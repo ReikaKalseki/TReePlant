744bcbf190888509eb37c79e90e6a22ba00b5408
Reika <reikasminecraft@gmail.com>
2017-07-02 01:54:19 -0400

    Update 4

--- config.lua
+++ config.lua
@@ -1,19 +1,19 @@
 Config = {}
 
 --Enable this to make repair packs work on trees.
-Config.treeRepair = true
+Config.treeRepair = settings.startup["tree-repair"].value--true
 
 --Enable this to disable attacks on trees - i.e. biters destroying trees and rocks to get to their target.
-Config.noTreeAttack = true
+Config.noTreeAttack = settings.startup["no-tree-attacks"].value--true
 
 --Enable this to make stumps last much longer than normal, so that you can repair them more easily
-Config.longLifeStumps = true
+Config.longLifeStumps = settings.startup["long-life-stumps"].value--true
 
 --Enable this to make the stumps mineable and drop their parent tree type, placeable anew. This is the core of the mod.
-Config.placeableTrees = true
+Config.placeableTrees = settings.startup["mineable-stumps"].value--true
 
 --Enable this to make stumps have bigger hitboxes to make mining them easier.
-Config.bigStumpHitbox = true
+Config.bigStumpHitbox = settings.startup["big-stumps"].value--true
 
 --Enable this to make trees drop themselves as well as their wood.
-Config.treesDropSelves = true
+Config.treesDropSelves = settings.startup["tree-drops"].value--true
